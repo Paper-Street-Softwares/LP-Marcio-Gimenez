8a10a20ff581143065eb3dd3ea07954a27176cd5
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-12 09:07:35 -0300

    fix: Insere mapa e formulário na mesma Seção

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -54,7 +54,7 @@ export default function Index() {
       <Steps colorMode={colorMode} />
       <BlogPosts />
       <Faq colorMode={colorMode} />
-      <Maps colorMode={colorMode} />
+      {/* <Maps colorMode={colorMode} /> */}
       <FooterSocial
         colorMode={colorMode}
         addresSecundario={false}
