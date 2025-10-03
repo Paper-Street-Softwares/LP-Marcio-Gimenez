4aa9a0ab5226fb5a90f089b59a4d12a53403c0c9
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-10-03 09:27:44 -0300

    fix: Copy hero adicionado e gap zerado na Feature

--- src/components/sections/FeaturesButton.jsx
+++ src/components/sections/FeaturesButton.jsx
@@ -56,7 +56,7 @@ export default function FeaturesButton({ colorMode }) {
           miniTitleTextColor="text-secondary"
         />
         <SectionWrapper>
-          <div className="flex flex-wrap justify-center desktop1:justify-evenly w-full gap-[36px] tablet1:gap-[24px] desktop1:w-[90%]">
+          <div className="flex flex-wrap justify-center desktop1:justify-evenly w-full gap-[36px] tablet1:gap-[24px] desktop1:gap-0 desktop1:w-[90%]">
             {[1, 2, 3, 4, 5, 6].map((i) => {
               const card = content.texts.features[`card${i}`];
               return (
